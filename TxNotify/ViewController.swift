1c1cc726f7ad2252fc24197ff80bd86c8dda9456
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-01-25 00:31:49 -0500

    Initial Commit

---
+++ TxNotify/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TxNotify
+//
+//  Created by Vivian Phung on 1/25/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
